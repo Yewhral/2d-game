f43433a32f64c6e742bf8499078a12ebae416225
Michal Zabrzyjewski <zabrzyjewski@gmail.com>
2026-05-15 16:20:08 +0200

    Preload menu images

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,36 @@ import { MainMenuOverlay } from "./components/MainMenuOverlay/MainMenuOverlay";
 import { PhaserCanvas } from "./components/PhaserCanvas/PhaserCanvas";
 import { useGameEvent } from "./hooks/useGameEvent";
 import { Loader } from "./components/MainMenuOverlay/Loader";
+import { useEffect } from "react";
+
+const UI_ASSETS_TO_PRELOAD = [
+  "/UI/ribbon_08_purple_rounded_left.png",
+  "/UI/ribbon_08_purple_rounded_middle.png",
+  "/UI/ribbon_08_purple_rounded_right.png",
+  "/UI/ribbon_02_teal_rounded_left.png",
+  "/UI/ribbon_02_teal_rounded_middle.png",
+  "/UI/ribbon_02_teal_rounded_right.png",
+  "/UI/ribbon_04_red_rounded_left.png",
+  "/UI/ribbon_04_red_rounded_middle.png",
+  "/UI/ribbon_04_red_rounded_right.png",
+  "/UI/ribbon_06_yellow_rounded_left.png",
+  "/UI/ribbon_06_yellow_rounded_middle.png",
+  "/UI/ribbon_06_yellow_rounded_right.png",
+  "/UI/ribbon_10_slate_rounded_left.png",
+  "/UI/ribbon_10_slate_rounded_middle.png",
+  "/UI/ribbon_10_slate_rounded_right.png",
+];
 
 export default function App() {
   const [scene, setScene] = useState("Boot");
+
+  // Preload UI assets in the background
+  useEffect(() => {
+    UI_ASSETS_TO_PRELOAD.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+    });
+  }, []);
 
   useGameEvent(
     "scene-changed",
